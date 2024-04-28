1207cca1b3b6ee039b7a36688c105405bbda1fd7
Matkulov <168366317+Matkulov@users.noreply.github.com>
2024-04-28 13:53:20 -0500

    Add files via upload

---
+++ lesson-8.py
@@ -0,0 +1,35 @@
+# word = "Hello,  World !!!"
+# print(word)
+# word ="Hello, world !!!"
+# print(word)
+#
+# word = " Hello world!!!"
+# print(word)
+# word = " HEllo world!!!"
+# print(word)
+#
+# word = ' Hello World!!!'
+# print(wor
+# word = " HEllo world!!!"
+# print(word)
+
+
+
+#
+# stix="Я к вам пишу — чего же боле? \n\
+# Что я могу еще сказать?\n\
+# Теперь, я знаю, в вашей воле\n\
+# Меня презреньем наказать."
+# print(stix)
+
+# stix=("Я к вам пишу — чего же боле?\n"
+# "Что я могу еще сказать?\n"
+# "Теперь, я знаю, в вашей воле\n"
+# "Меня презреньем наказать.")
+# print(stix)
+
+stix='''Я к вам пишу — чего же боле? 
+Что я могу еще сказать?
+Теперь, я знаю, в вашей воле
+Меня презреньем наказать.'''
+print(stix)
---
+++ lesson9.py
@@ -0,0 +1,24 @@
+# s= "c\d\new folder"
+# print(s)
+# s= r"c\d\new folder"
+# print(s)
+# s1 ='Hello, '
+# s2 ='wordl!'
+# s3= (s1 + s2)
+# print(s3 )
+#
+
+# name = 'John'
+# age = 22
+#
+# print("My name is" + name + str(age) )
+
+
+
+
+
+
+
+
+
+
